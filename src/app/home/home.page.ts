71e7a6d868883f6b7bd88586dbe1c13fd853e875
Nora <nsansaturnino@birt.eus>
2022-01-31 11:47:40 +0100

    Solucion

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,5 @@
+import { CuestionarioService } from './../servicios/cuestionario.service';
+import { IPregunta } from './../interfaces/interfaces';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public cuestionario: CuestionarioService) {
+  }
 
+  public onClick(item: IPregunta) {
+    this.cuestionario.responder(item);
+  }
+
+  public guardar() {
+    this.cuestionario.guardar();
+  }
 }
